20aba6f4b87c3da39400995cb5bfc8efaa989414
qaisarnadeem3313 <71261528+qaisarnadeem3313@users.noreply.github.com>
2022-01-19 00:06:22 +0500

    Update script.js

--- Project 1/script.js
+++ Project 1/script.js
@@ -4,23 +4,60 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const cpassword = document.getElementById('cpassword');
 
+
+// Function to update class and message for errors
 function showError(input, message){
+    //Get the parent element of the input field (.form-control)
     const formControl = input.parentElement;
-    formControl.className = 'form-control error'
-    const small = formControl.querySelector('small')
+    //Override the class- add error
+    formControl.className = 'form-control error';
+    //Get the small element for the error message
+    const small = formControl.querySelector('small');
+    // Override the text for small element using the input message
     small.innerText = message;
 }
 
-
+// Function to update class for success
+function showSuccess(input){
+//Get the parent element of the input field (.form-control)
+const formControl = input.parentElement;
+// Replace the class - add success
+formControl.className = 'form-control success';
+}
 
 //Event listeners
 //Create Event Listene for submit button
 form.addEventListener('submit', function(e){
     // stop page from reloading on submit
     e.preventDefault();
+   // Check to see if the fields are empty
     //Check if username input is empty
     if(username.value===''){
         showError(username,'username is required');
+    } else{
+        showSuccess(username);
     }
+
+    //Check if email input is empty
+    if(email.value===''){
+        showError(email,'email is required');
+    } else{
+        showSuccess(email);
+    }
+
+    //Check if password input is empty
+    if(password.value===''){
+        showError(password,'password is required');
+    } else{
+        showSuccess(password);
+    }
+
+    //Check if cpassword input is empty
+    if(cpassword.value===''){
+        showError(cpassword,'password is required');
+    } else{
+        showSuccess(cpassword);
+    }
+
     console.log(username.value);
 });
